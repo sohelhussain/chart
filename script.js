5caa0f4441fc1bca1e5d771ef71bc7e4dc62b56d
sohelhussain <sohelhussaing@gmail.com>
2024-02-20 21:56:13 -0800

    uploading a file

---
+++ script.js
@@ -0,0 +1,13 @@
+// first step is show the real product 
+const product = [
+    {name: "Wooden Stoll", headline: "Made with apple wood", price: "$700", image: "https://images.unsplash.com/photo-1503602642458-232111445657?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {name: "White Chair", headline: "Soft like cloud", price: "$1300", image: "https://images.unsplash.com/photo-1567538096630-e0c55bd6374c?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {name: "Broun Caouch", headline: "this is a relaxing area", price: "$3400", image: "https://images.unsplash.com/photo-1619596662481-085e45d69762?q=80&w=3164&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
+]
+const popular = []
+
+let clutter = 
+
+product.forEach( items => {
+    
+})
